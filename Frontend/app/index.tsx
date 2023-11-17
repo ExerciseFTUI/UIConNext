349dd9345e084c66ffdb349b594d13f557ed1997
RadityaDito <raditya.ihsan@ui.ac.id>
2023-11-17 16:38:15 +0700

    Fix Tailwind Issue

---
+++ Frontend/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Card from "../components/Card";
+
+export default function Home() {
+  return (
+    <View className="">
+      <Text className="text-lime-500">Homes</Text>
+      <Card />
+    </View>
+  );
+}
