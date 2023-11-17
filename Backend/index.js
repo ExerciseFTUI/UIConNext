112ba01cc163c1e660da1cd83360096e3de27856
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-11-17 17:09:14 +0700

    add bodyParser for vercel

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./src/config/db");
 const authRoutes = require("./src/routes/Auth.routes");
+const bodyParser = require("body-parser");
 
 const app = express();
 dotenv.config();
@@ -10,6 +11,8 @@ db.connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.CORS_ORIGINS.split(", "),
