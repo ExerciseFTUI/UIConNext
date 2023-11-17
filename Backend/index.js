d83f1d6fa152e653bbd4ac88040a3fabd11cc63e
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-11-17 14:01:08 +0700

    add backend

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const db = require("./src/config/db");
+const authRoutes = require("./src/routes/Auth.routes");
+
+const app = express();
+dotenv.config();
+db.connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGINS.split(", "),
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
+
+// Routes used in the app
+app.use("/auth", authRoutes);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
