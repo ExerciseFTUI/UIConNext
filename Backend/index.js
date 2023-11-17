58686fdd4872dff70c04e2692ab6ac5914cd49d7
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-11-17 17:27:50 +0700

    last cleanup

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const db = require("./src/config/db");
 const authRoutes = require("./src/routes/Auth.routes");
-// const bodyParser = require("body-parser");
 
 const app = express();
 dotenv.config();
@@ -11,8 +10,6 @@ db.connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.CORS_ORIGINS.split(", "),
@@ -23,7 +20,6 @@ app.use(
 
 // Routes used in the app
 app.use("/auth", authRoutes);
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
