547e3f10442a34a6075288c57fc4f29b9f6495d8
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-11-24 14:35:25 +0700

    Try to remove env

--- Frontend/app/(main)/home.tsx
+++ Frontend/app/(main)/home.tsx
@@ -10,7 +10,7 @@ import {
   Image,
   Button,
 } from "react-native";
-import { AntDesign, FontAwesome5, Entypo } from "@expo/vector-icons";
+import { AntDesign, FontAwesome5, Entypo, Ionicons } from "@expo/vector-icons";
 import Tweet from "../../components/Tweet";
 import Profile from "../../components/modals/Profile";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -18,7 +18,8 @@ import { useRouter } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
 
 export default function App() {
-  const [isTriggered, setIsTriggered] = useState(false);
+  const [isTriggered, setIsTriggered] = useState(false);  
+  const [createTriggered, setCreateTriggered] = useState(false);
 
   const { signOut } = useAuth();
   const doLogout = () => {
@@ -30,45 +31,50 @@ export default function App() {
     <>
       {/* <SafeAreaView style={{ flex: 0, backgroundColor: "#739072" }} /> */}
       <SafeAreaView className="flex-1 items-center ">
-        {/* Header */}
-        <View className="px-5 items-center w-screen h-14 bg-light_green flex flex-row justify-between rounded-b-lg">
-          <Text className="text-2xl text-primary font-bold"> UI ConNext</Text>
-          <TouchableOpacity className=" border-dark_green border rounded-lg p-3 bg-dark_green">
-            <Text className="text-white" onPress={() => setIsTriggered(true)}>
-              Click Here
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity className=" border-dark_green border rounded-lg p-3 bg-dark_green">
-            <Text className="text-white" onPress={() => doLogout()}>
-              Logout
-            </Text>
-          </TouchableOpacity>
-        </View>
-        {/* End of Header */}
+            {/* Header */}
+            <View className="px-5 items-center w-screen h-14 bg-light_green flex flex-row justify-between rounded-b-lg">
+                <Text className="text-2xl text-primary font-bold"> UI ConNext</Text>
+                <TouchableOpacity onPress={() => setIsTriggered(true)} className=" border-dark_green border rounded-lg p-3 bg-dark_green"> 
+                    <Text className="text-white"  >Click Here</Text>
+                </TouchableOpacity>
+            </View>
+            {/* End of Header */}
 
-        {/* <TouchableOpacity className=" border-dark_green border rounded-lg p-3 bg-dark_green">
-          <Text className="text-white" onPress={() => router.push("/login")}>
-            Click Here
-          </Text>
-        </TouchableOpacity> */}
-        <ScrollView className="mt-2">
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
-          <Tweet></Tweet>
+            <ScrollView showsVerticalScrollIndicator={false} className=' w-full px-3'>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                
+                {/* Modal User */}
+                <Profile 
+                status={isTriggered}
+                setStatus={setIsTriggered} />
+                {/* End of Modal User */}
 
-          {/* Modal User */}
-          <Profile status={isTriggered} setStatus={setIsTriggered} />
-          {/* End of Modal User */}
+                <StatusBar style="auto" />
+            </ScrollView>
 
-          <StatusBar style="auto" />
-        </ScrollView>
-      </SafeAreaView>
+            {/* Button create Oink */}
+            <TouchableOpacity onPress={() => setCreateTriggered((prev) => !prev)} className=' absolute bottom-14 right-7 flex justify-center items-center rounded-full bg-white border-light_green border-2 w-16 h-16'>
+                <Ionicons name="create" size={30} color="#3A4D39" />
+            </TouchableOpacity>
+            {/* End of Button create Oink */}
+
+            {/* Choice View */}
+            {createTriggered && 
+            <View className="w-20 h-20 bg-yellow-300 absolute bottom-0" >
+
+            </View>
+            }
+            {/* End Choice View */}
+
+        </SafeAreaView>
     </>
   );
 }
