68f570f323409a77f4554ae2b9d1690c402c77aa
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-11-24 15:22:33 +0700

    Revisi

--- Frontend/app/(main)/home.tsx
+++ Frontend/app/(main)/home.tsx
@@ -10,7 +10,7 @@ import {
   Image,
   Button,
 } from "react-native";
-import { AntDesign, FontAwesome5, Entypo } from "@expo/vector-icons";
+import { AntDesign, FontAwesome5, Entypo, Ionicons } from "@expo/vector-icons";
 import Tweet from "../../components/Tweet";
 import Profile from "../../components/modals/Profile";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -19,6 +19,7 @@ import { useAuth } from "@clerk/clerk-expo";
 
 export default function App() {
   const [isTriggered, setIsTriggered] = useState(false);
+  const [createTriggered, setCreateTriggered] = useState(false);
 
   const { signOut } = useAuth();
   const doLogout = () => {
@@ -33,13 +34,13 @@ export default function App() {
         {/* Header */}
         <View className="px-5 items-center w-screen h-14 bg-light_green flex flex-row justify-between rounded-b-lg">
           <Text className="text-2xl text-primary font-bold"> UI ConNext</Text>
-          <TouchableOpacity className=" border-dark_green border rounded-lg p-3 bg-dark_green">
-            <Text className="text-white" onPress={() => setIsTriggered(true)}>
+          <TouchableOpacity onPress={() => setIsTriggered(true)} className=" border-dark_green border rounded-lg p-3 bg-dark_green">
+            <Text className="text-white" >
               Click Here
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity className=" border-dark_green border rounded-lg p-3 bg-dark_green">
-            <Text className="text-white" onPress={() => doLogout()}>
+          <TouchableOpacity onPress={() => doLogout()} className=" border-dark_green border rounded-lg p-3 bg-dark_green">
+            <Text className="text-white" >
               Logout +
             </Text>
           </TouchableOpacity>
@@ -51,7 +52,7 @@ export default function App() {
             Click Here
           </Text>
         </TouchableOpacity> */}
-        <ScrollView className="mt-2">
+        <ScrollView showsVerticalScrollIndicator={false} className=' w-full px-3'>
           <Tweet></Tweet>
           <Tweet></Tweet>
           <Tweet></Tweet>
@@ -68,6 +69,20 @@ export default function App() {
 
           <StatusBar style="auto" />
         </ScrollView>
+
+        {/* Button create Oink */}
+        <TouchableOpacity onPress={() => setCreateTriggered((prev) => !prev)} className=' absolute bottom-14 right-7 flex justify-center items-center rounded-full bg-white border-light_green border-2 w-16 h-16'>
+            <Ionicons name="create" size={30} color="#3A4D39" />
+        </TouchableOpacity>
+        {/* End of Button create Oink */}
+            
+        {createTriggered && 
+        <View className="w-20 h-20 bg-yellow-300 absolute bottom-0" >
+
+        </View>
+        }
+        {/* End Choice View */}
+
       </SafeAreaView>
     </>
   );
