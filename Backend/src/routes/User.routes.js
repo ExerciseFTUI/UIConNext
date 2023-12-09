0e1969f73e9fc53d87972e2a145f1d6c8236aad6
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-12-09 15:12:37 +0700

    change tmp dir

--- Backend/src/routes/User.routes.js
+++ Backend/src/routes/User.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 
 const userControllers = require('../controllers/User.controllers');
-const upload = multer({ dest: 'tmp/' });
+const upload = multer({ dest: '/tmp/' });
 
 router.post('/uploadImage', upload.single('file'), userControllers.uploadImage);
 
