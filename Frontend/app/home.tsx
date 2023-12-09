ef608841e2a2f164b6a04094c48b8d92b5d19a1d
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-12-10 00:19:07 +0700

    Nambah dikit buat component create oink

--- Frontend/app/home.tsx
+++ Frontend/app/home.tsx
@@ -1,61 +1,76 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
-  Modal,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Image,
-  Button,
+    Modal,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Image,
+    Button,
 } from "react-native";
 import { AntDesign, FontAwesome5, Entypo, Ionicons } from "@expo/vector-icons";
 import Tweet from "../components/Tweet";
 import Profile from "../components/modals/Profile";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CreateOink from "../components/modals/post/CreateOink";
 
 export default function App() {
-  const [isTriggered, setIsTriggered] = useState(false);
+    const [profileTriggered, setProfileTriggered] = useState(false);
+    const [createTriggered, setCreateTriggered] = useState(false);
 
-  return (
+    return (
     <>
-      {/* <SafeAreaView style={{ flex: 0, backgroundColor: "#739072" }} /> */}
-      <SafeAreaView className="flex-1 items-center ">
-        {/* Header */}
-        <View className="px-5 items-center w-screen h-14 bg-light_green flex flex-row justify-between rounded-b-lg">
-            <Text className="text-2xl text-primary font-bold"> UI ConNext</Text>
-            <TouchableOpacity onPress={() => setIsTriggered(true)} className=" border-dark_green border rounded-lg p-3 bg-dark_green"> 
-                <Text className="text-white"  >Click Here</Text>
+        {/* <SafeAreaView style={{ flex: 0, backgroundColor: "#739072" }} /> */}
+        <SafeAreaView className="flex-1 items-center ">
+            {/* Header */}
+            <View className="px-5 items-center w-screen h-14 bg-light_green flex flex-row justify-between rounded-b-lg">
+                <Text className="text-2xl text-primary font-bold"> UI ConNext</Text>
+                <TouchableOpacity onPress={() => setProfileTriggered(true)} className=" border-dark_green border rounded-lg p-3 bg-dark_green"> 
+                    <Text className="text-white"  >Click Here</Text>
+                </TouchableOpacity>
+            </View>
+            {/* End of Header */}
+
+            <ScrollView showsVerticalScrollIndicator={false} className=' w-full px-3'>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                <Tweet></Tweet>
+                
+                {/* Modal User */}
+                <Profile 
+                status={profileTriggered}
+                setStatus={setProfileTriggered} />
+                {/* End of Modal User */}
+
+                <StatusBar style="auto" />
+            </ScrollView>
+
+            {/* Button create Oink */}
+            <TouchableOpacity onPress={() => setCreateTriggered((prev) => !prev)} className=' absolute bottom-14 right-7 flex justify-center items-center rounded-full bg-white border-light_green border-2 w-16 h-16'>
+                {createTriggered ? 
+                    <AntDesign name="close" size={30} color="#3A4D39" />
+                    : 
+                    <Ionicons name="create" size={30} color="#3A4D39" />
+                }
             </TouchableOpacity>
-        </View>
-        {/* End of Header */}
+            {/* End of Button create Oink */}
 
-        <ScrollView className=' w-full px-3'>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            <Tweet></Tweet>
-            
-            {/* Modal User */}
-            <Profile 
-            status={isTriggered}
-            setStatus={setIsTriggered} />
-            {/* End of Modal User */}
+            {/* Choice View */}
+            {createTriggered && 
+            <CreateOink/>
+            // <View className="w-20 h-20 bg-yellow-300 absolute bottom-0" >
 
-          <StatusBar style="auto" />
-        </ScrollView>
-
-        {/* Button create Oink */}
-        <TouchableOpacity className=' absolute bottom-14 right-7 flex justify-center items-center rounded-full bg-white border-light_green border-2 w-16 h-16'>
-            <Ionicons name="create" size={30} color="#3A4D39" />
-        </TouchableOpacity>
-        {/* End of Button create Oink */}
+            // </View>
+            }
+            {/* End Choice View */}
 
         </SafeAreaView>
         </>
---
+++ Frontend/components/modals/post/CreateOink.tsx
@@ -0,0 +1,45 @@
+import { View, Text, Modal, Image, TextInput } from "react-native";
+import { AntDesign, FontAwesome5, Entypo } from "@expo/vector-icons";
+import { useState } from "react";
+
+const CreateOink = () => {
+  const [oinkMessages, onChangeOinkMessages] = useState('');
+  return (
+    <>
+        <View className=" rounded-2xl w-5/6 min-h-[30vh] bg-white absolute items-center top-32">
+          {/* Header */}
+          <View className=" flex flex-row rounded-t-2xl bg-yellow-100 w-full px-6 justify-between py-3 items-center">
+            <View className=" flex flex-row items-center">
+              <Image source={require("../../../assets/Avatar2.webp")} className="w-8 h-8 border border-white rounded-full mr-1"></Image>
+              <View>
+                <Text className=" font-semibold text-sm">Username</Text>
+                <Text className=" text-xs">Engineering</Text>
+              </View>
+            </View>
+            <Text>Oink!</Text>
+          </View>
+          {/* End of Header */}
+
+          {/* Body input */}
+          <View className=" w-full h-fit px-6 py-2">
+            <TextInput
+              onChangeText={onChangeOinkMessages}
+              value={oinkMessages}
+              placeholder="Create an oink in here . . ."
+              keyboardType="default"
+              className=" text-base text-black"
+            />
+          </View>
+          {/* End of Body input */}
+
+          {/* Footer */}
+          <View className=" bg-yellow-100 rounded-b-2xl w-full h-12 bottom-0">
+
+          </View>
+          {/* End of Footer */}
+        </View>
+    </>
+  )
+}
+
+export default CreateOink
